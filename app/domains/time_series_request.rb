4185f318604e29d7c66946e21d683ed258055254
bTspm <btspm10@gmail.com>
2020-05-18 22:11:53 -0400

    Use Constant.

--- app/domains/time_series_request.rb
+++ app/domains/time_series_request.rb
@@ -1,5 +1,6 @@
 class TimeSeriesRequest
   DAY1 = "1day".freeze
+  DATE_FORMAT = "%Y-%m-%d %H:%M:%S"
   DEFAULT_INTERVAL = DAY1
 
   attr_reader :end_date_with_time,
@@ -16,20 +17,20 @@ class TimeSeriesRequest
 
   def to_options
     {
-      end_date_with_time: @end_date_with_time.strftime("%Y-%m-%d %H:%M:%S"),
-      interval: @interval,
-      start_date_with_time: @start_date_with_time.strftime("%Y-%m-%d %H:%M:%S"),
-      symbol: @symbol
+     end_date_with_time: @end_date_with_time.strftime(DATE_FORMAT),
+     interval: @interval,
+     start_date_with_time: @start_date_with_time.strftime(DATE_FORMAT),
+     symbol: @symbol
     }
   end
 
   def self.five_year(symbol)
     end_date = DateTime.now.end_of_day
     args = {
-      end_date_with_time: end_date,
-      start_date_with_time: end_date - 6.years,
-      interval: DAY1,
-      symbol: symbol,
+     end_date_with_time: end_date,
+     start_date_with_time: end_date - 6.years,
+     interval: DAY1,
+     symbol: symbol,
     }
 
     new(args)
