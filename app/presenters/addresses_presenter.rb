4ec5b06a44e953bb86ace9c1b147030f085ccc38
bTspm <btspm10@gmail.com>
2020-06-07 03:53:04 -0400

    Address Presenter Refactor.

--- app/presenters/addresses_presenter.rb
+++ app/presenters/addresses_presenter.rb
@@ -8,21 +8,17 @@ class AddressesPresenter
       text = ""
       text += h.content_tag(:div, line_1) if line_1.present?
       text += h.content_tag(:div, line_2) if line_2.present?
-      text += h.content_tag(:div, _city_state_country_and_zip) if _city_state_country_and_zip
+      text +=  _city_state_country_and_zip
       text.html_safe
     end
 
     private
 
     def _city_state_country_and_zip
-      return if state.blank? && country.blank? && zip_code.blank?
+      return "" if state.blank? && country.blank? && zip_code.blank?
 
-      address = ""
-      address += "#{city}" if city.present?
-      address += ", #{state}" if state.present?
-      address += ", #{country}" if country.present?
-      address += ", #{zip_code}" if zip_code.present?
-      address
+      address = [city, state, country, zip_code].compact.join(", ")
+      h.content_tag(:div, address)
     end
   end
 end
