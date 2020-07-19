794a0d98c19ac382e34a5044e6ada65de388e36a
bTspm <btspm10@gmail.com>
2020-07-19 04:14:44 -0400

    - Update address presenter with state & country objects.

--- app/presenters/addresses_presenter.rb
+++ app/presenters/addresses_presenter.rb
@@ -2,6 +2,9 @@ class AddressesPresenter
   include Btspm::Presenters::Presentable
 
   class Scalar < Btspm::Presenters::ScalarPresenter
+    delegate  :code, to: :country, prefix: true
+    delegate  :name, to: :state, prefix: true, allow_nil: true
+
     def formatted
       return "N/A" if data_object.blank?
 
@@ -17,7 +20,7 @@ class AddressesPresenter
     def _city_state_country_and_zip
       return "" if state.blank? && country.blank? && zip_code.blank?
 
-      address = [city, state, country, zip_code].compact.join(", ")
+      address = [city, state_name, country_code, zip_code].compact.join(", ")
       h.content_tag(:div, address)
     end
   end
