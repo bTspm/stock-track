0fdc384118b79f5c518f2a9e684a9b2edca474c2
bTspm <btspm10@gmail.com>
2021-05-03 22:18:27 -0400

    Fix: Barchart Client JS Scraping to HTML.

--- app/models/concerns/scraper/bar_chart_client.rb
+++ app/models/concerns/scraper/bar_chart_client.rb
@@ -1,62 +1,33 @@
 module Scraper
-  class BarChartClient < BaseScraper
+  class BarChartClient < ::BaseClient
     CURRENT_RATING_INDEX = 3
-    PRICE_TARGETS_COUNT = 3
 
     def rating_by_symbol(symbol)
-      get_response do
-        @browser.goto("https://www.barchart.com/stocks/quotes/#{symbol}/analyst-ratings")
-        @browser
-          .element(class: "chart-header")
-          .wait_until(&:present?)
-        response = Nokogiri::HTML(@browser.html)
+      url = "https://www.barchart.com/stocks/quotes/#{symbol}/analyst-ratings"
+      response = Nokogiri::HTML(get(url).body).css("div.average-column")[CURRENT_RATING_INDEX].children
 
-        args = {
-          analysts_count: _analysts(response),
-          original_rating: _rating(response),
-          price_target: _price_target(response),
-          url: @browser.url,
-          source: Entities::ExternalAnalyses::Analysis::BAR_CHART
-        }
+      args = {
+        analysts_count: _analysts(response),
+        original_rating: _rating(response),
+        url: url,
+        source: Entities::ExternalAnalyses::Analysis::BAR_CHART
+      }
 
-        Entities::ExternalAnalyses::Analysis.new(args)
-      end
+      Entities::ExternalAnalyses::Analysis.new(args)
     end
 
     private
 
     def _analysts(response)
       response
-        .css("div.average-column")[CURRENT_RATING_INDEX]
-        .children
         .css("span.bold")
         .map(&:text)
         .join
         .to_i
     end
 
-    def _price_target(response)
-      high, average, low = response
-                             .css("div.chart-header")
-                             .map(&:text)
-                             .first
-                             .split(/(\n)/)
-                             .reject(&:empty?)
-                             .map(&:to_float)
-                             .compact
-                             .take(PRICE_TARGETS_COUNT)
-
-      {
-        average: average,
-        high: high,
-        low: low
-      }
-    end
-
     def _rating(response)
       response
-        .css("div.average-column")[CURRENT_RATING_INDEX]
-        .children
         .css("div.block__colored-header")
         .map(&:text)
         .join("")
--- spec/models/concerns/scrapper/bar_chart_client_spec.rb
+++ spec/models/concerns/scrapper/bar_chart_client_spec.rb
@@ -1,58 +1,33 @@
 require "rails_helper"
 
 describe Scraper::BarChartClient do
-  let!(:browser) { double(:browser) }
+  let!(:conn) { double(:conn) }
+  let(:response) { double(:response, body: body) }
   subject(:client) { described_class.new }
 
   before do
-    allow(Watir::Browser).to receive(:new) { browser }
-    allow(browser).to receive(:goto).with(url) { "Browser Call" }
+    allow(Faraday).to receive(:new) { conn }
+    expect(client).to receive(:get).with(url) { response }
   end
 
   describe "#rating_by_symbol" do
+    let(:symbol) { "AAPL" }
+    let(:url) { "https://www.barchart.com/stocks/quotes/#{symbol}/analyst-ratings" }
     let(:analysis_args) do
       {
         analysts_count: 31,
         original_rating: "Strong Buy",
-        price_target: { average: 2417.9, high: 2953.0, low: 2056.0 },
         source: Entities::ExternalAnalyses::Analysis::BAR_CHART,
-        url: "URL"
+        url: url
       }
     end
-    let(:class_element) { double(:class_element) }
-    let(:html) { html_fixture("/external_analyses/bar_chart.html") }
-    let(:symbol) { double(:symbol) }
-    let(:url) { "https://www.barchart.com/stocks/quotes/#{symbol}/analyst-ratings" }
+    let(:body) { html_fixture("/external_analyses/bar_chart.html") }
     subject { client.rating_by_symbol(symbol) }
 
-    context "when call is successful" do
-      it "expect to initialize an analysis" do
-        expect(browser).to receive(:element).with(
-          class: "chart-header"
-        ) { class_element }
-        expect(class_element).to receive(:wait_until)
-        expect(browser).to receive(:html) { html }
-        expect(Entities::ExternalAnalyses::Analysis).to receive(:new).with(analysis_args) { "Analysis" }
-        expect(browser).to receive(:url) { "URL" }
-        expect(browser).to receive(:close) { "Closed" }
+    it "expect to init analysis" do
+      expect(Entities::ExternalAnalyses::Analysis).to receive(:new).with(analysis_args) { "Analysis" }
 
-        expect(subject).to eq "Analysis"
-      end
-    end
-
-    context "when call is a failure" do
-      subject do
-        client.rating_by_symbol(symbol)
-      rescue StandardError
-        "Browser closed after exception"
-      end
-
-      it "expect to close the browser when an exception is raised" do
-        expect(browser).to receive(:element).and_raise StandardError
-        expect(browser).to receive(:close) { "Closed" }
-
-        subject
-      end
+      expect(subject).to eq "Analysis"
     end
   end
 end
