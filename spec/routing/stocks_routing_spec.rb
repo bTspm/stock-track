1da2b806b2399f579149b1c6035a05e7a56883a1
bTspm <btspm10@gmail.com>
2021-06-13 12:58:25 -0400

    Test: Remove additional route tests.

--- spec/routing/stocks_routing_spec.rb
+++ spec/routing/stocks_routing_spec.rb
@@ -4,20 +4,6 @@ describe StocksController do
   let(:base_url) { "/stocks" }
   let(:controller) { "stocks" }
 
-  describe "#earnings" do
-    let(:action) { "earnings" }
-    subject { get("#{base_url}/#{action}") }
-
-    it { is_expected.to route_to(controller: controller, action: action) }
-  end
-
-  describe "#growth" do
-    let(:action) { "growth" }
-    subject { get("#{base_url}/#{action}") }
-
-    it { is_expected.to route_to(controller: controller, action: action) }
-  end
-
   describe "#home" do
     let(:action) { "home" }
     subject { get("#{base_url}/#{action}") }
@@ -27,27 +13,6 @@ describe StocksController do
 
   describe "#information" do
     let(:action) { "information" }
-    subject { get("#{base_url}/#{action}") }
-
-    it { is_expected.to route_to(controller: controller, action: action) }
-  end
-
-  describe "#news" do
-    let(:action) { "news" }
-    subject { get("#{base_url}/#{action}") }
-
-    it { is_expected.to route_to(controller: controller, action: action) }
-  end
-
-  describe "#quote" do
-    let(:action) { "quote" }
-    subject { get("#{base_url}/#{action}") }
-
-    it { is_expected.to route_to(controller: controller, action: action) }
-  end
-
-  describe "#stats" do
-    let(:action) { "stats" }
     subject { get("#{base_url}/#{action}") }
 
     it { is_expected.to route_to(controller: controller, action: action) }
